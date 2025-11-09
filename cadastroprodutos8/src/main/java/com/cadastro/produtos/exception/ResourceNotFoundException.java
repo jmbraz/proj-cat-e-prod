4b0064af3300689ebb7be8b6d02f13b3a1e78adc
rodrigovieira1234a-spec <rodrigovieira1234a@gmail.com>
2025-11-09 14:54:55 -0300

    Delete cadastroprodutos8 directory

--- cadastroprodutos8/src/main/java/com/cadastro/produtos/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cadastro.produtos.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
--- cadastroprodutos8/src/main/java/com/cadastro/produtos/exception/ValidationException.java
+++
@@ -1,5 +0,0 @@
-package com.cadastro.produtos.exception;
-
-public class ValidationException {
-
-}
